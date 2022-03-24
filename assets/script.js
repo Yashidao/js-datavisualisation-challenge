810daeb451b3276a2b5f8e15ae6bc5e0aff3d2f2
Elena Kozyreva <elena.kozyreva.prof@gmail.ru>
2022-03-24 12:13:46 +0100

    terminer graph pour tableu 2

--- assets/script.js
+++ assets/script.js
@@ -11,43 +11,78 @@ canvasPolice.setAttribute("id", "canvasPolice")
 document.getElementById("table1").insertAdjacentElement("beforebegin", canvasPolice)
 
 
-
 let canvasHomicide = document.createElement("canvas");
 canvasHomicide.setAttribute("id", "canvasHomicide");
 document.getElementById("table2").insertAdjacentElement("beforebegin", canvasHomicide);
 
+ 
+const table = document.getElementById('table2');
+
+let country = [];
+let values2 = [];
+let values1 = [];
+
+// Itération sur les cellules
+for (let i = 1; i < table.rows.length; i++) {
+    country.push(table.rows[i].cells[1].innerHTML);
+    values1.push(table.rows[i].cells[2].innerHTML);
+    values2.push(table.rows[i].cells[3].innerHTML);
+    
+}
+
+// console.log(country);
+// console.log(values1);
+// console.log(values2);
 
 let myChart = new Chart(canvasHomicide, {
-  type: 'bar',
-  data: {
-    labels: [
-    'first',
-    'second',
-    '3',
-    '4',
-    '5',
-  ],
-  datasets: [{
-      label: 'My First dataset',
-      color: 'green',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      barPercentage: 0.5,
-      barThickness: 16,
-      maxBarThickness: 88,
-      minBarLength: 2,
-      data: [10, 20, 30, 40, 50]
-    }]
-
-  },
-  
-});
-
-
-
-
+     data: {
+      labels: country, 
+      datasets: [{
+        type: 'bar',
+        label: '2007-2009', 
+        data: values1, 
+        backgroundColor: 'rgba(255, 99, 132, 0.7)',
+        borderColor: [ 
+          'rgba(255,99,132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 2 // Définir la largeur du trait de section
+      }, {
+        type: 'bar',
+        label: '2010-2012', 
+        data: values2, 
+        backgroundColor: 'rgba(255, 206, 86, 0.7)',
+        borderColor: [ 
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1 
+      }]
+    },
+    
+  });
 
 // TEST
 canvasH1.style.backgroundColor = 'red'
 canvasPolice.style.backgroundColor = 'red'
-// canvasHomicide.style.backgroundColor = 'red'
+canvasHomicide.style.backgroundColor = 'rgba(75, 192, 192, 0.2)'
+// let xhr = new XMLHttpRequest;
+
+// xhr.open('GET', 'https://canvasjs.com/services/data/datapoints.php', true);
+// xhr.onload = function() 
+//   {
+//     if (this.status === 200) {
+//       let cites = JSON.parse(this.responseText);
+//       console.log(cites);
+//       // citeText.textContent = cites.quote;
+//       // document.querySelector('h2').textContent = cites.author;
+      
+//     } else {
+//       alert('Erreur!')
+//     }
+//   }
+// xhr.send();
+
